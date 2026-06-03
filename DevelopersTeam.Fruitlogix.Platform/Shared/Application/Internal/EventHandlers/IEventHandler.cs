fec897c33cedc2ed97eb334d01a224808d28e4ba
ECEB0704 <gogli.chavez@gmai.com>
2026-06-03 10:25:11 -0500

    fix(shared): add missing newline at end of IEventHandler.cs

--- DevelopersTeam.Fruitlogix.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
+++ DevelopersTeam.Fruitlogix.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -3,4 +3,4 @@ using Cortex.Mediator.Notifications;
 namespace DevelopersTeam.Fruitlogix.Platform.Shared.Application.Internal.EventHandlers;
 public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
 {
-}
+} 
