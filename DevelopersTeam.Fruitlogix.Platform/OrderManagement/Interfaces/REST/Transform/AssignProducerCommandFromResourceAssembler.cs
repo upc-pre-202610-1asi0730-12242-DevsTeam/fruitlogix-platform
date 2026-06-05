dd1ff12e8f51f8a947cf8b18c1f23541d5c42eb5
ECEB0704 <gogli.chavez@gmail.com>
2026-06-04 21:07:17 -0500

    feat(order-management): add order management bounded context

---
+++ DevelopersTeam.Fruitlogix.Platform/OrderManagement/Interfaces/REST/Transform/AssignProducerCommandFromResourceAssembler.cs
@@ -0,0 +1,10 @@
+﻿using DevelopersTeam.Fruitlogix.Platform.OrderManagement.Domain.Model.Commands;
+using DevelopersTeam.Fruitlogix.Platform.OrderManagement.Interfaces.REST.Resources;
+
+namespace DevelopersTeam.Fruitlogix.Platform.OrderManagement.Interfaces.REST.Transform;
+
+public static class AssignProducerCommandFromResourceAssembler
+{
+    public static AssignProducerCommand ToCommandFromResource(int orderId, AssignProducerResource resource) =>
+        new(orderId, resource.ProducerId);
+}
