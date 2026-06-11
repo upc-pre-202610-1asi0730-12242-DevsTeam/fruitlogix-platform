3d78ec8deae14dc00dc445c6467183dc16ff0a8f
Daril <u202317338@upc.edu.pe>
2026-06-11 14:34:30 -0500

    feat: implement GetAllIncidents query and corresponding query service in IncidentsController in BC Quality Control

---
+++ DevelopersTeam.Fruitlogix.Platform/QualityControl/Application/QueryServices/IIncidentQueryService.cs
@@ -0,0 +1,9 @@
+﻿using DevelopersTeam.Fruitlogix.Platform.QualityControl.Domain.Model.Aggregates;
+using DevelopersTeam.Fruitlogix.Platform.QualityControl.Domain.Model.Queries;
+
+namespace DevelopersTeam.Fruitlogix.Platform.QualityControl.Application.QueryServices;
+
+public interface IIncidentQueryService
+{
+    Task<IEnumerable<Incident>> Handle(GetAllIncidentsQuery query);
+}
