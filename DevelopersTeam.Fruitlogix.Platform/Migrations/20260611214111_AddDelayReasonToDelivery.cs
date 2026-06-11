13786e93b4b0db5211315abaf8bb4bb34ac828ba
ECEB0704 <gogli.chavez@gmail.com>
2026-06-11 16:41:51 -0500

    chore: add migration AddDelayReasonToDelivery

---
+++ DevelopersTeam.Fruitlogix.Platform/Migrations/20260611214111_AddDelayReasonToDelivery.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevelopersTeam.Fruitlogix.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDelayReasonToDelivery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "delay_reason",
+                table: "deliveries",
+                type: "longtext",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "delay_reason",
+                table: "deliveries");
+        }
+    }
+}
