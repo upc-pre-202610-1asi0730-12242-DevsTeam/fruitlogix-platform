1b19f98e64f462d8ba3f0ff1b13b3fb3d3253dde
Sergi9017 <u202211295@upc.edu.pe>
2026-06-11 18:55:26 -0500

    feat: implement GetAllInvoicesQuery and corresponding query service in InvoicesController

---
+++ DevelopersTeam.Fruitlogix.Platform/PaymentManagement/Application/QueryServices/IInvoiceQueryService.cs
@@ -0,0 +1,9 @@
+﻿using DevelopersTeam.Fruitlogix.Platform.PaymentManagement.Domain.Model.Aggregates;
+using DevelopersTeam.Fruitlogix.Platform.PaymentManagement.Domain.Model.Queries;
+
+namespace DevelopersTeam.Fruitlogix.Platform.PaymentManagement.Application.QueryServices;
+
+public interface IInvoiceQueryService
+{
+    Task<IEnumerable<Invoice>> Handle(GetAllInvoicesQuery query);
+}
