d3d369a3fca2c9d3d931ac6677a49e960a6c5cc1
ECEB0704 <gogli.chavez@gmail.com>
2026-06-11 17:25:28 -0500

    chore: add migration for alerts in bc logistics

---
+++ DevelopersTeam.Fruitlogix.Platform/Migrations/20260611222454_AddLogisticsAlerts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DevelopersTeam.Fruitlogix.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLogisticsAlerts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "alerts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    delivery_id = table.Column<int>(type: "int", nullable: false),
+                    type = table.Column<string>(type: "longtext", nullable: false),
+                    severity = table.Column<string>(type: "longtext", nullable: false),
+                    message = table.Column<string>(type: "longtext", nullable: false),
+                    is_resolved = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true),
+                    updated_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_alerts", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "alerts");
+        }
+    }
+}
