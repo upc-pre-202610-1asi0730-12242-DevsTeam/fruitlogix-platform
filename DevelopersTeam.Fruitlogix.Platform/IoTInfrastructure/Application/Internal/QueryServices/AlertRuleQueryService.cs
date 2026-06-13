e3ff0bda72f4797356b9acab9782fcf5d1efe112
ECEB0704 <gogli.chavez@gmail.com>
2026-06-13 18:04:55 -0500

    feat: add ep get alerts rules bc iot

---
+++ DevelopersTeam.Fruitlogix.Platform/IoTInfrastructure/Application/Internal/QueryServices/AlertRuleQueryService.cs
@@ -0,0 +1,13 @@
+﻿using DevelopersTeam.Fruitlogix.Platform.IoTInfrastructure.Application.QueryServices;
+using DevelopersTeam.Fruitlogix.Platform.IoTInfrastructure.Domain.Model.Aggregates;
+using DevelopersTeam.Fruitlogix.Platform.IoTInfrastructure.Domain.Model.Queries;
+using DevelopersTeam.Fruitlogix.Platform.IoTInfrastructure.Domain.Repositories;
+
+namespace DevelopersTeam.Fruitlogix.Platform.IoTInfrastructure.Application.Internal.QueryServices;
+
+public class AlertRuleQueryService(IAlertRuleRepository repository)
+    : IAlertRuleQueryService
+{
+    public async Task<IEnumerable<AlertRule>> Handle(GetAllAlertRulesQuery query)
+        => await repository.ListAsync();
+}
